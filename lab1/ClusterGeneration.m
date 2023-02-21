24634e72f400bff29e05e6f4e697892553b9697b
Oceane Vandame <oceanevandame@Oceanes-MacBook-Pro.local>
2023-02-21 14:55:39 -0500

    edited cluster generation

--- lab1/ClusterGeneration.m
+++ lab1/ClusterGeneration.m
@@ -1,5 +1,6 @@
-% data
-% case 1
+% Given data
+
+% Case 1
 A_size = 200;
 A_mean = [5; 10];
 A_cov = [8 0; 0 4];
@@ -8,7 +9,7 @@ B_size = 200;
 B_mean = [10; 15];
 B_cov = [8 0; 0 4];
 
-% case 2
+% Case 2
 C_size = 100;
 C_mean = [5; 10];
 C_cov = [8 4; 4 40];
@@ -21,24 +22,9 @@ E_size = 150;
 E_mean = [10; 5];
 E_cov = [10 -5; -5 20];
 
-% random number generation
-% since bivariate, two arrays need to be generated
-% corresponding entries in each array will be coordinates for each point
-A_x1 = randn(A_size, 1);
-A_x2 = randn(A_size, 1);
-
-B_x1 = randn(B_size, 1);
-B_x2 = randn(B_size, 1);
-
-C_x1 = randn(C_size, 1);
-C_x2 = randn(C_size, 1);
-
-D_x1 = randn(D_size, 1);
-D_x2 = randn(D_size, 1);
-
-E_x1 = randn(E_size, 1);
-E_x2 = randn(E_size, 1);
-
-% randn cluster generation
-%R_A = chol(A_cov);
-%A_dist = repmat(A_mean,10,1) + randn(A_size,1)*R_A
+% Create bivariate normal distributions for each class
+class_A = bivariatenormalfunct(A_size, A_cov, A_mean);
+class_B = bivariatenormalfunct(B_size, B_cov, B_mean);
+class_C = bivariatenormalfunct(C_size, C_cov, C_mean);
+class_D = bivariatenormalfunct(D_size, D_cov, D_mean);
+class_E = bivariatenormalfunct(E_size, E_cov, E_mean);
---
+++ lab1/stdcontour.m
@@ -0,0 +1,6 @@
+% will edit this
+function [eigVec, eigVal] = stdcontour(sigma)
+    [eigVec, eigVal] = eig(sigma);
+end
+
+
