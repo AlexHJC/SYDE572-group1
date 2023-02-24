4e132bbf9d6e101ae0e9623c30fd3e1fff9c1058
ocevandame <ocevandame@gmail.com>
2023-02-24 12:37:27 -0500

    fixed contour plots

--- lab1/std_contour.m
+++ lab1/std_contour.m
@@ -1,20 +1,15 @@
 function [theta, a, b] = std_contour(sigma)
     [eigVec, eigVal] = eig(sigma);
-    %theta = max(max(atan(eigVec(:,1)/eigVec(:,2))));
+
     if eigVal(1,1) > eigVal(2,2)
-        theta = atan(eigVal(2,1)/eigVal(1,1));
+        theta = atan(eigVec(2,1)/eigVec(1,1));
         a = eigVal(1,1);
         b = eigVal(2,2);
     else
-        theta = atan(eigVal(2,2)/eigVal(1,2));
+        theta = atan(eigVec(2,2)/eigVec(1,2));
         a = eigVal(2,2);
         b = eigVal(1,1);
         
-    %theta = max(atan2(eigVec(:,1),eigVec(:,2)));
-    %a = max(max(eigVal));
-    %b = min(max(eigVal));
-    %(1,1);
-    %b = eigVal(2,2);
     end
 end
 
