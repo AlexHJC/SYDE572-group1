cde7905c0bd86a3d87876620792552b89cd565f3
AlexHJC <Alexcho.jun@gmail.com>
2023-03-02 17:56:38 -0500

    added nearest neighbor function and driver to main.m, changed MED and MAP boundary plot to dotted line

--- lab1/functions/NN2.m
+++ lab1/functions/NN2.m
@@ -1,8 +1,7 @@
-function [NNdist] = NN1(k, X1, X2, classA, classB)
+function [NNdist] = NN2(k, X1, X2, classA, classB, classC)
+%NN Calculate the K Nearest Neighbours for two or three classes
 
-%NN Calculate the K Nearest Neighbours for two classes
-
-% Defintion: For a given x you wish to classify, you compute the distance
+% Definition: For a given x you wish to classify, you compute the distance
 % between x and all labeled samples, and you assign x the same class as its
 % nearest neighbour
 
@@ -17,18 +16,23 @@ for i = 1:size(X1, 1)
         point = [X1(i,j), X2(i,j)];
         a_distances = sqrt(sum((classA - point).^2, 2));
         b_distances = sqrt(sum((classB - point).^2, 2));
+        c_distances = sqrt(sum((classC - point).^2, 2));
         
         % find the k smallest distances and calculate their means
         [~, sorted_a] = sort(a_distances);
         mean_a = mean(a_distances(sorted_a(1:k)));
         [~, sorted_b] = sort(b_distances);
         mean_b = mean(b_distances(sorted_b(1:k)));
+        [~, sorted_c] = sort(c_distances);
+        mean_c = mean(c_distances(sorted_c(1:k)));
         
         % assign the label of the nearest neighbor
-        if mean_a > mean_b
+        if mean_a > mean_b && mean_a > mean_c
             NNdist(i,j) = 1;
+        elseif mean_b > mean_a && mean_b > mean_c
+            NNdist(i,j) = 2;
         else
-            NNdist(i,j) = 0;
+            NNdist(i,j) = 3;
         end
         
     end
--- lab1/main.m
+++ lab1/main.m
@@ -113,8 +113,14 @@ ged_grid = create_mesh_grid(X1_1, X2_1);
 ged_applied = GED_clf(ged_grid, X1_1, X2_1, A_mean, B_mean, A_cov, B_cov);
 
 % Classifying with NN
-NN_clf = NN1(1,X1_1, X2_1, class_A, class_B);
-kNN_clf = NN1(5,X1_1, X2_1, class_A, class_B);
+
+% Case 1:
+NN_clf1 = NN1(1,X1_1, X2_1, class_A, class_B);
+kNN_clf1 = NN1(5,X1_1, X2_1, class_A, class_B);
+
+% Case 2:
+NN_clf2 = NN2(1,X1_2, X2_2, class_A, class_B, class_C);
+kNN_clf2 = NN2(5,X1_2, X2_2, class_A, class_B, class_C);
 
 % Classify each point with MAP
 % Create MAP grid
@@ -141,14 +147,14 @@ map3_applied = MAP3_clf(map3_grid, X1_2, X2_2, C_mean, D_mean, E_mean, C_cov, D_
 % Case 1 -- MED, GED, MICD & MAP Classifiers
 figure(3)
 hold on
-title('Case 1: MED, GED, MICD & MAP Classifiers')
+title('Case 1: MED, GED/MICD & MAP Classifiers')
 scatter(class_A(:,1), class_A(:,2), 'red', DisplayName='Class A', Marker='.')
 scatter(class_B(:,1), class_B(:,2), 'blue', DisplayName='Class B', Marker='.')
 plot_ellipse(A_mean(1), A_mean(2), thetaA, aA, bA, 'red', 'Std. dev contour for Class A')
 plot_ellipse(B_mean(1), B_mean(2), thetaB, aB, bB, 'blue', 'Std. dev contour for Class B')
 contour(X1_1, X2_1, med_applied, "black", DisplayName='MED decision boundary') % MED CLASSIFIER
-contour(X1_1, X2_1, ged_applied, "green", DisplayName='GED decision boundary') % GED CLASSIFIER
-contour(X1_1, X2_1, map_applied, "blue", DisplayName='MAP decision boundary') % MAP CLASSIFIER
+contour(X1_1, X2_1, ged_applied, "green", DisplayName='GED decision boundary') % GED/MICD CLASSIFIER
+contour(X1_1, X2_1, map_applied, "--blue", DisplayName='MAP decision boundary') % MAP CLASSIFIER
 xlabel('x1')
 ylabel('x2')
 legend('Location', 'northeast')
@@ -158,12 +164,12 @@ hold off
 figure(4)
 hold on
 title('Case 1: NN & 5NN Classifiers')
-scatter(class_A(:,1), class_A(:,2), 'red', DisplayName='Class A', Marker='.')
-scatter(class_B(:,1), class_B(:,2), 'blue', DisplayName='Class B', Marker='.')
+scatter(class_A(:,1), class_A(:,2), 'red', DisplayName='Class A')
+scatter(class_B(:,1), class_B(:,2), 'blue', DisplayName='Class B')
 plot_ellipse(A_mean(1), A_mean(2), thetaA, aA, bA, 'red', 'Std. dev contour for Class A')
 plot_ellipse(B_mean(1), B_mean(2), thetaB, aB, bB, 'blue', 'Std. dev contour for Class B')
-contour(X1_1, X2_1, NN_clf, DisplayName='NN decision boundary') % NN CLASSIFIER
-contour(X1_1, X2_1, kNN_clf, DisplayName='kNN decision boundary') % NN CLASSIFIER
+contour(X1_1, X2_1, NN_clf1, DisplayName='NN decision boundary') % NN CLASSIFIER
+contour(X1_1, X2_1, kNN_clf1, DisplayName='kNN decision boundary') % NN CLASSIFIER
 xlabel('x1')
 ylabel('x2')
 legend('Location', 'northeast')
@@ -173,7 +179,7 @@ hold off
 % Case 2 -- MED, GED, MICD & MAP Classifiers
 figure(5)
 hold on
-title('Case 2: MED, GED, MICD & MAP Classifiers')
+title('Case 2: MED, GED/MICD & MAP Classifiers')
 scatter(class_C(:,1), class_C(:,2), 'red', DisplayName='Class C', Marker='.')
 scatter(class_D(:,1), class_D(:,2), 'blue', DisplayName='Class D', Marker='.')
 scatter(class_E(:,1), class_E(:,2), 'green', DisplayName='Class E', Marker='.')
@@ -181,8 +187,8 @@ plot_ellipse(C_mean(1), C_mean(2), thetaC, aC, bC, 'red', 'Std. dev contour for 
 plot_ellipse(D_mean(1), D_mean(2), thetaD, aD, bD, 'blue', 'Std. dev contour for Class D')
 plot_ellipse(E_mean(1), E_mean(2), thetaE, aE, bE, 'green', 'Std. dev contour for Class E')
 contour(X1_2, X2_2, med3_applied, "black", DisplayName='MED decision boundary') % MED CLASSIFIER
-contour(X1_2, X2_2, ged3_applied, "green", DisplayName='GED decision boundary') % GED CLASSIFIER
-contour(X1_2, X2_2, map3_applied, "blue", DisplayName='MAP decision boundary') % MAP CLASSIFIER
+contour(X1_2, X2_2, ged3_applied, "green", DisplayName='GED decision boundary') % GED/MICD CLASSIFIER
+contour(X1_2, X2_2, map3_applied, "--blue", DisplayName='MAP decision boundary') % MAP CLASSIFIER
 xlabel('x1')
 ylabel('x2')
 legend('Location', 'northeast')
@@ -192,19 +198,17 @@ hold off
 figure(6)
 hold on
 title('Case 2: NN & 5NN Classifiers')
-scatter(class_C(:,1), class_C(:,2), 'red', DisplayName='Class C', Marker='.')
-scatter(class_D(:,1), class_D(:,2), 'blue', DisplayName='Class D', Marker='.')
-scatter(class_E(:,1), class_E(:,2), 'green', DisplayName='Class E', Marker='.')
+scatter(class_C(:,1), class_C(:,2), 'red', DisplayName='Class C')
+scatter(class_D(:,1), class_D(:,2), 'blue', DisplayName='Class D')
+scatter(class_E(:,1), class_E(:,2), 'green', DisplayName='Class E')
 plot_ellipse(C_mean(1), C_mean(2), thetaC, aC, bC, 'red', 'Std. dev contour for Class C')
 plot_ellipse(D_mean(1), D_mean(2), thetaD, aD, bD, 'blue', 'Std. dev contour for Class D')
 plot_ellipse(E_mean(1), E_mean(2), thetaE, aE, bE, 'green', 'Std. dev contour for Class E')
+contour(X1_2, X2_2, NN_clf2, DisplayName='NN decision boundary') % NN CLASSIFIER
+contour(X1_2, X2_2, kNN_clf2, DisplayName='kNN decision boundary') % NN CLASSIFIER
 xlabel('x1')
 ylabel('x2')
 legend('Location', 'northeast')
 hold off
-
-
-a=1;
-
 
 
