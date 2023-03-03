f89975f532aca4b3821588a37131dd911219b1d0
AlexHJC <Alexcho.jun@gmail.com>
2023-03-03 18:06:13 -0500

    added student number

--- lab1/functions/GED_error_analysis.m
+++ lab1/functions/GED_error_analysis.m
@@ -4,8 +4,8 @@
 
 % populate two column vectors with the class A and B true positives (TA, TB) and the
 % False positives (FA, FB)
-[TA, FA] = get_error(A_size, GED_clf(ged_grid, X1_1, X2_1, A_mean, B_mean, A_cov, B_cov), @(d) d < 0);
-[TB, FB] = get_error(B_size, GED_clf(ged_grid, X1_1, X2_1, A_mean, B_mean, A_cov, B_cov), @(d) d > 0);
+[TA, FA] = get_true_false(A_size, GED_clf(ged_grid, X1_1, X2_1, A_mean, B_mean, A_cov, B_cov), @(d) d < 0);
+[TB, FB] = get_true_false(B_size, GED_clf(ged_grid, X1_1, X2_1, A_mean, B_mean, A_cov, B_cov), @(d) d > 0);
 
 % combine column vectors into 2x2 confusion matrix
 GED_conf_matrix = [
