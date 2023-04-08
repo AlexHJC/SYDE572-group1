abb876695f0844206388efd327872ea244a726e1
AlexHJC <Alexcho.jun@gmail.com>
2023-04-08 15:12:37 -0400

    copied section 4 files from example

---
+++ lab2/Section4.m
@@ -0,0 +1,53 @@
+%% Section 4: Sequential discriminants
+
+clear;
+close all;
+load('lab2_3.mat');
+
+A_data4 = load('lab2_3','a');
+B_data4 = load('lab2_3', 'b');
+classA_part4 = A_data4.a;
+classB_part4 = B_data4.b;
+
+%% system set-up
+x_min = min([a(:,1); b(:,1)]);
+x_max = max([a(:,1); b(:,1)]);
+y_min = min([a(:,2); b(:,2)]);
+y_max = max([a(:,2); b(:,2)]);
+step = 1;
+[x, y] = meshgrid(x_min-2:step:x_max+2, y_min-1:step:y_max+2);
+grid = zeros(size(x));
+
+%% Compute Sequential Classifiers
+% [4.1, 4.2]
+[G1, err1] = sequential_classifier(x, y, 0, classA_part4, classB_part4);
+[G2, err2] = sequential_classifier(x, y, 0, classA_part4, classB_part4);
+[G3, err3] = sequential_classifier(x, y, 0, classA_part4, classB_part4);
+
+%% 3 sequential classifiers
+
+% G1
+figure;
+scatter(classA_part4(:,1), classA_part4(:,2), 'ro');
+hold on;
+scatter(classB_part4(:,1), classB_part4(:,2), 'kx');
+hold on;
+contour(x,y,G1,'c');
+title('Sequential Classifier 1');
+legend('Class A','Class B', 'Sequential Classifier');
+% legend({'Class A','Class B', 'Sequential Classifier'},'location' , 'southeast');
+xlabel('x1');
+ylabel('x2');
+hold off;
+
+%% G2
+figure;
+scatter(classA_part4(:,1), classA_part4(:,2), 'ro');
+hold on;
+scatter(classB_part4(:,1),classB_part4(:,2), 'kx');
+hold on;
+contour(x,y,G2,'c');
+title('Sequential Classifier 2');
+legend('Class A','Class B', 'Sequential Classifier');
+xlabel('x1');
+ylabel('x2');
